21c9aa8d26d9fd33de96ea3cef9ddecb5b404e33
Blaze1TZ <79504292+Blaze1TZ@users.noreply.github.com>
2021-05-25 08:35:29 +0200

    Add files via upload

---
+++ JS/image-slider.js
@@ -0,0 +1,23 @@
+var imageCounter = 1;
+
+showDiv(imageCounter) ;
+
+function changeImage(m) {
+    showDiv(imageCounter = imageCounter + m);
+}
+
+function showDiv(n) {
+    var i;
+    var imageArray = document.getElementsByClassName("mySlides");
+    if (n > imageArray.length) {
+        imageCounter = 1;
+    }
+    if (n < 1) {
+        imageCounter = imageArray.length;
+    }
+    for (i = 0; i < imageArray.length; i++) {
+        imageArray[i].style.dispaly = "none";
+    }
+    imageArray[imageCounter - 1].style.dispaly = "block";
+}
+
